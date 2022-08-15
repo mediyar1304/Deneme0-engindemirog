0fa7b2b8578a3ee46ca785b88220213f90984d0c
Mediyar Bedirhan DENİZ <90138633+mediyar1304@users.noreply.github.com>
2022-08-15 13:49:47 +0000

    Update

---
+++ donguler.py
@@ -0,0 +1,22 @@
+krediler = ["Hızlı kredi", "Maaşını Halkbank'tan alanlara özel","Mutlu emekli ihtiyaç kredisi"]
+
+
+# Alias ==> takma ad
+for kredi in krediler:
+  print(kredi)
+
+
+for i in range(len(krediler)):
+  print(krediler[i])
+
+for i in range (10):
+  print(i)
+
+for i in range (3,10):
+  print(i)
+
+for i in range (0,11,2):
+  print(i)
+
+for kredi in krediler:
+  print("<option>"+ kredi + "<option>")
---
+++ main.py
@@ -0,0 +1,6 @@
+krediler = ["Hızlı kredi", "Maaşını Halkbank'tan alanlara özel","Mutlu emekli ihtiyaç kredisi"]
+
+
+# Alias ==> takma ad
+for kredi in krediler:
+  print("<option>"+ kredi + "<option>")
